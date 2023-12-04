e1435db6451015772d9898caef87514c86dffe8b
깃kkh333 <k2ho33333@gmail.com>
2023-12-04 21:38:20 +0900

    마무리

---
+++ sbb/sbb/src/main/java/com/mysite/sbb/article/Article.java
@@ -0,0 +1,30 @@
+package com.mysite.sbb.article;
+
+import com.mysite.sbb.user.SiteUser;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 100)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    private LocalDateTime modifyDate;
+
+    @ManyToOne
+    private SiteUser author;
+}

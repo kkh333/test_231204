a60f20d30299ba684d8fc9fa79625a4ddfd91f19
깃kkh333 <k2ho33333@gmail.com>
2024-02-29 03:40:13 +0900

    d

--- sbb/sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ sbb/sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -10,13 +10,5 @@ class SbbApplicationTests {
 	@Autowired
 	private ArticleService articleService;
 
-	@Test
-	void testJpa() {
-		for (int i = 1; i <= 300; i++) {
-			String subject = String.format("테스트 데이터입니다:[%03d]", i);
-			String content = "내용무";
-			this.articleService.create(subject, content, null);
-		}
-	}
 
 }

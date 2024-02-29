7a410feeda8948e73e38e29be3b7146ed768f729
kkh <k2ho33333@gmail.com>
2024-02-29 11:57:43 +0900

    asd

--- sbb/sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ sbb/sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,14 +1,8 @@
 package com.mysite.sbb;
 
-import com.mysite.sbb.article.ArticleService;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SbbApplicationTests {
-	@Autowired
-	private ArticleService articleService;
-
 
 }
